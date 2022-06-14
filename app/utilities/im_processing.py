2bedff8627349bdf0d5895aba9be521901c65b74
Hernosepo <90519142+Hernosepo@users.noreply.github.com>
2022-06-14 16:47:56 -0500

    PR (#6)
    Corrected a typo. Changed "|" to "," in Union.

--- app/utilities/im_processing.py
+++ app/utilities/im_processing.py
@@ -6,10 +6,9 @@ from typing import Union
 from PIL import Image, ImageTk  # type: ignore[import]
 
 
-def load_img(path, size: Union[tuple[int] | float] = None):
+def load_img(path, size: Union[tuple[int],float] = None):
     """
     This should load images.
-
     The size parameter can be:
         A tuple of ints, for resizing;
         A float, for indicating a percentage to resize; or
@@ -27,4 +26,6 @@ def load_img(path, size: Union[tuple[int] | float] = None):
         new_size = tuple(int(length * size) for length in img.size)
         return ImageTk.PhotoImage(img.resize(new_size))
 
-    raise ValueError("Expected size to be either a tuble of ints, a float or None.")
+    
+
+    raise ValueError("Expected size to be either a tuple of ints, a float or None.")
