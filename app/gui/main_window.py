0f070a94b0cf9765ff794168b6e90442ba7437a3
ArielMAJ <ariel.maj@hotmail.com>
2022-07-23 19:21:21 -0300

    Worked on app buttons: added icons. Changed relative paths so main_window can be run directly.
    Some other minor changes such as changes to docstrings, comments, removed unused commented code, etc.

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -2,23 +2,23 @@
 This module implements the app's main window class.
 """
 
-# from sys import platform
 import os
 import json
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import customtkinter as ctk  # type: ignore[import]
 
-from utilities import im_processing, im_bg  # type: ignore[import]
+try:
+    from utilities.im_processing import load_img  # type: ignore[import]
+    from utilities.im_bg import rm_bg, rm_bg_from_folder  # type: ignore[import]
+    from utilities.paths import join_pr  # type: ignore[import]
+except ImportError:
+    import sys
 
-# try:
-#     import top_levels
-# except ImportError:
-#     from . import top_levels
-
-# from time import sleep
-# from PIL import Image, ImageTk
-# import threading
+    sys.path.insert(0, os.path.abspath(".."))
+    from utilities.im_processing import load_img  # type: ignore[import]
+    from utilities.im_bg import rm_bg, rm_bg_from_folder  # type: ignore[import]
+    from utilities.paths import join_pr  # type: ignore[import]
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
@@ -34,20 +34,9 @@ class MainWindow(ctk.CTk):
         self.widgets: dict = {}
         self.settings: dict = {}
         self.vars: dict = {}
-
+        self.images: dict = {}
         # self.font = ("Arial", 12)
-        # self.colors: dict[str, str] = {
-        #     "yellow": "#FCE388",
-        #     "blue": "#C2CEFF",
-        #     "red": "#B52C30",
-        #     "green": "#256A49",
-        #     "gray": "#525a49",
-        # }
 
-        # self.colors["text_color"] = self.colors["gray"]
-        # self.colors["app_bg_color"] = self.colors["yellow"]
-
-        # self.after(0, self._load_window)
         self._load_window()
 
     def _load_window(self):
@@ -56,7 +45,6 @@ class MainWindow(ctk.CTk):
         """
         # self.withdraw()
         self._basic_configs()
-        # self._place_menu_bar()
         self._place_widgets()
         self._place_window_on_screen()
         # self.deiconify()
@@ -69,12 +57,18 @@ class MainWindow(ctk.CTk):
         """
         self._check_and_load_settings()
 
+        self.images["icon"] = load_img(join_pr("images", "icon.png"), (30, 30))
+        self.images["upload"] = load_img(join_pr("images", "upload.png"), (40, 40))
+        self.images["sync"] = load_img(join_pr("images", "sync.png"), (40, 40))
+        self.images["search"] = load_img(join_pr("images", "search.png"), (40, 40))
+        self.images["bg"] = load_img(join_pr("images", "background.png"), (40, 40))
+        self.images["template"] = load_img(join_pr("images", "layout.png"), (40, 40))
+
         self.title(self.settings["title"])
-        self.iconphoto(False, im_processing.load_img("./images/icon.png", (30, 30)))
+        self.iconphoto(False, self.images["icon"])
         # self.minsize(width=800, height=600)
         self.resizable(width=False, height=False)
 
-        # self.config(padx=20, pady=30, bg=self.colors["app_bg_color"])
         self.config(padx=20, pady=30)
 
         self.vars["folder_path"] = tk.StringVar()
@@ -88,10 +82,10 @@ class MainWindow(ctk.CTk):
         This function will load the app's current settings. If it doesn't exist, it should
         partially create a settings file with some defaults.
         """
+
+        resources = join_pr("resources", "settings.json")
         try:
-            with open(
-                "./resources/settings.json", "r", encoding="utf-8"
-            ) as settings_file:
+            with open(resources, encoding="utf-8") as settings_file:
                 self.settings = json.load(settings_file)
         except FileNotFoundError:
             self.settings = {
@@ -100,91 +94,14 @@ class MainWindow(ctk.CTk):
                 "pady": 1,
                 "title": "",
             }
-            if not os.path.exists("./resources"):
-                os.mkdir("./resources")
-            with open(
-                "./resources/settings.json", "w", encoding="utf-8"
-            ) as settings_file:
+            if not os.path.exists(folder_path := join_pr("resources")):
+                os.mkdir(folder_path)
+            with open(resources, "w", encoding="utf-8") as settings_file:
                 json.dump(self.settings, settings_file, indent=4)
-
-    # def _place_menu_bar(self):
-    #     """
-    #     This function should handle the menu bar creation.
-    #     """
-    #     # The Menu-Bar object ("the whole strip", without buttons just yet).
-    #     self.widgets["menu_bar"] = tk.Menu(self)
-    #     self.config(menu=self.widgets["menu_bar"])
-
-    #     ###################################################################
-    #     # The menu-widgets/buttons will be created below.
-    #     # They can be either an actual button with a command or have an
-    #     # associated submenu with buttons (or more submenus).
-
-    #     ###################################################################
-    #     # Options button in menu (it will open a submenu).
-    #     self.widgets["menu_options"] = tk.Menu(self.widgets["menu_bar"], tearoff="off")
-
-    #     # Adding exit button (menu-widget with a command).
-    #     self.widgets["menu_options"].add_command(
-    #         label="Sair",  # Leave
-    #         command=self.destroy,
-    #     )
-
-    #     ###################################################################
-    #     # Images/rembg button in menu.
-    #     self.widgets["menu_images"] = tk.Menu(self.widgets["menu_bar"], tearoff="off")
-    #     # Creating sub menu for image segmentation.
-    #     self.widgets["sub_menu_segmentation"] = tk.Menu(
-    #         self.widgets["menu_images"],
-    #         tearoff="off",
-    #     )
-
-    #     # Adding sub_menu_segmentation to menu_images
-    #     self.widgets["menu_images"].add_cascade(
-    #         label="Remover fundos",
-    #         menu=self.widgets["sub_menu_segmentation"],
-    #     )
-
-    #     # Buttuns inside submenu
-    #     self.widgets["sub_menu_segmentation"].add_command(
-    #         label="Pessoas/Rostos",
-    #         command=self.remove_background_button_press,
-    #     )
-    #     self.widgets["sub_menu_segmentation"].add_command(
-    #         label="Roupas",
-    #         command=lambda: self.remove_background_button_press(
-    #             model_name="cloth_segm_u2net_latest", alpha_matting=False
-    #         ),
-    #     )
-    #     self.widgets["sub_menu_segmentation"].add_command(
-    #         label="Etc",
-    #         command=lambda: self.remove_background_button_press(model_name="u2net"),
-    #     )
-
-    #     ###################################################################
-    #     # Adding widgets (menu buttons) to menu bar.
-
-    #     self.widgets["menu_bar"].add_cascade(
-    #         label="Opções",  # Options
-    #         menu=self.widgets["menu_options"],
-    #     )
-
-    #     self.widgets["menu_bar"].add_cascade(
-    #         label="Imagens",  # Images
-    #         menu=self.widgets["menu_images"],
-    #     )
-
-    #     self.widgets["menu_bar"].add_command(
-    #         label="Configurações",  # Settings
-    #         command=lambda: messagebox.showinfo(
-    #             title="To Be Implemented (placeholder)",
-    #             message="A top level should open here and show the settings",
-    #         ),
-    #     )
 
     def _place_widgets(self):
         """
-        This function should handle the non-menubar widgets.
+        This function should handle creating and placing most of the widgets.
         """
 
         self.widgets["lbl_folder"] = ctk.CTkLabel(
@@ -223,21 +140,26 @@ class MainWindow(ctk.CTk):
 
         self.widgets["btn_search_folder"] = ctk.CTkButton(
             self,
-            text="Mudar pasta",  # Change folder
+            text="MUDAR        ",  # Change folder
+            image=self.images["search"],
             command=self.search_folder_button_press,
+            corner_radius=5,
         )
         self.widgets["btn_search_folder"].grid(
             row=0,
             column=2,
             padx=self.settings["padx"],
             pady=self.settings["pady"],
+            ipady=self.settings["pady"] * 3,
             sticky="NEWS",
         )
 
         self.widgets["btn_rembg_FOLDER"] = ctk.CTkButton(
             self,
             # Remove background (all subfolders)
-            text="Remover fundos\n(todas subpastas)",
+            text="FUNDOS      ",
+            image=self.images["bg"],
+            corner_radius=5,
             command=self.folder_remove_background_button_press,
         )
         self.widgets["btn_rembg_FOLDER"].grid(
@@ -245,6 +167,7 @@ class MainWindow(ctk.CTk):
             column=2,
             padx=self.settings["padx"],
             pady=self.settings["pady"],
+            ipady=self.settings["pady"] * 3,
             sticky="NEWS",
         )
 
@@ -266,10 +189,11 @@ class MainWindow(ctk.CTk):
         )
 
         self.widgets["btn_montar"] = ctk.CTkButton(
-            text="Montar Templates",  # "Assemble" templates.
-            # image=img_start,
+            text="TEMPLATES",  # "Assemble" templates.
+            image=self.images["template"],
             # padx=self.settings["padx"],
             # border=0,
+            corner_radius=5,
             # bg=self.colors["app_bg_color"],
             # activebackground=self.colors["app_bg_color"],
             # command=...,
@@ -279,6 +203,7 @@ class MainWindow(ctk.CTk):
             row=3,
             padx=self.settings["padx"],
             pady=self.settings["pady"],
+            ipady=self.settings["pady"] * 3,
             sticky="NEWS",
         )
 
@@ -300,9 +225,11 @@ class MainWindow(ctk.CTk):
         )
 
         self.widgets["btn_upload"] = ctk.CTkButton(
-            text="Fazer Upload",  # Upload images.
-            # image=img_start,
+            text="UPLOAD      ",  # Upload images.
+            image=self.images["upload"],
+            compound="left",
             # padx=self.settings["padx"],
+            corner_radius=5,
             # border=0,
             # bg=self.colors["app_bg_color"],
             # activebackground=self.colors["app_bg_color"],
@@ -314,13 +241,16 @@ class MainWindow(ctk.CTk):
             row=4,
             padx=self.settings["padx"],
             pady=self.settings["pady"],
+            ipady=self.settings["pady"] * 3,
             sticky="NEWS",
         )
 
         self.widgets["btn_refresh"] = ctk.CTkButton(
-            text="Atualizar",  # Refresh .
-            # image=img_start,
+            text="ATUALIZAR ",  # Refresh .
+            image=self.images["sync"],
+            compound="left",
             # padx=self.settings["padx"],
+            corner_radius=5,
             # border=0,
             # bg=self.colors["app_bg_color"],
             # activebackground=self.colors["app_bg_color"],
@@ -331,6 +261,7 @@ class MainWindow(ctk.CTk):
             row=1,
             padx=self.settings["padx"],
             pady=self.settings["pady"],
+            ipady=self.settings["pady"] * 3,
             sticky="NEWS",
         )
 
@@ -357,7 +288,9 @@ class MainWindow(ctk.CTk):
         return "center" if len(self.vars["folder_path"].get()) <= 85 else "w"
 
     def remove_background_button_press(
-        self, model_name: str = "u2net_human_seg", alpha_matting: bool = False
+        self,
+        model_name: str = "u2net_human_seg",
+        alpha_matting: bool = False,
     ) -> None:
         """
         Implementation of the background removing button.
@@ -382,7 +315,7 @@ class MainWindow(ctk.CTk):
             return
 
         for img_file_path in selected_files:
-            im_bg.rm_bg(
+            rm_bg(
                 img_file_path,
                 model_name=model_name,
                 alpha_matting=alpha_matting,
@@ -407,7 +340,7 @@ class MainWindow(ctk.CTk):
         - Images should start with numbers 1 to 3;
         - They shouldn't end in "G" (e.g. 1_IMG will be read but 1_IMG_NO_BG.jpg won't);
         """
-        im_bg.rm_bg_from_folder(self.settings["default_path_to_images"])
+        rm_bg_from_folder(self.settings["default_path_to_images"])
 
         messagebox.showinfo(
             # Finished
@@ -437,8 +370,8 @@ class MainWindow(ctk.CTk):
 
         self._check_and_load_settings()
         self.settings["default_path_to_images"] = selected_folder
-        with open("./resources/settings.json", "w", encoding="utf-8") as settings_file:
-            json.dump(self.settings, settings_file, indent=4)
+        with open(join_pr("resources", "settings.json"), "w", encoding="utf-8") as file:
+            json.dump(self.settings, file, indent=4)
 
         self.vars["folder_path"].set(selected_folder)
         self.widgets["lbl_folder_path"].configure(anchor=self._path_anchor)
@@ -446,9 +379,8 @@ class MainWindow(ctk.CTk):
 
 def main() -> int:
     """
-    This function exists for easily testing this module "directly". This will skip  the loading
-    screen. To get this function to run, you'll need to run this module as a "package" from the
-    same folder as main.py (e.g.: py -m gui.main_window), otherwise it won't run.
+    This function exists for easily testing this module "directly". This will skip the loading
+    screen.
     """
     root = MainWindow()
     root.mainloop()
